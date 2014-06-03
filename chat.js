898c402926e56c443a90f70fdc066d77c3488c3f
changhan <changhanlee@gmail.com>
2014-06-03 10:40:13 -0400

    shit

--- chat.js
+++ chat.js
@@ -1,10 +1,18 @@
-var bird = new Circle(20,40,20);
+
+var bird = new Circle(20,40,20)
+
 bird.addTo(stage);
+
 bird.fill('white');
+
 bird.animate('2s',{
 	rotation: 0,
 	x: 200
 });
-stage.on('key', function(e) {
-  console.log('Key event: ' + e.keyCode);
+
+
+
+
+stage.on('click',function(e){
+	bird.fill('orange');
 });
