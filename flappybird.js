204492913a8cda4ced46d5a7a164aac446a83a48
changhan <changhanlee@gmail.com>
2014-06-04 17:53:13 -0400

    shit

--- flappybird.js
+++ flappybird.js
@@ -1,12 +1,21 @@
-var intro = new Text('frappy brrr\n',
-					'type a for jump\n');
+stage.x = 1000;
+stage.y = 1000;
+
+console.log(stage.x);
+console.log(stage.y);
+
+/*
+var intro = new Text('frappy brrr');
 intro.addTo(stage).attr({
   fontFamily: 'Arial, sans-serif',
   fontSize: '14',
   textFillColor: 'white',
 });
+*/
+var end = new Text('over');
 
-var highpipe = new Rect (250,0,50,400);
+var highpipeypos = 0;
+var highpipe = new Rect (250,highpipeypos,50,400);
 highpipe.addTo(stage);
 highpipe.fill('green');
 
@@ -21,11 +30,12 @@ bird.x = 15;
 bird.y = 500;
 
 
-var gravity = new Animation('8s',{
-	y:800
+var gravity = new Animation('1s',{
+	y:1000
 });
 gravity.addSubjects([bird]);
 gravity.play();
+
 
 
 var jump = new Animation('1s',{
@@ -42,3 +52,15 @@ stage.on('keydown', function(e) {
     }
   });
 
+
+highpipeypos = 300;;
+
+if(bird.y == 1000){
+	end.addTo(stage).attr({
+		fontFamily: 'Arial, sans-serif',
+		fontSize: '14',
+		textFillColor: 'white',
+	});
+}
+
+console.log(bird.y);
--- prompt.js
+++
@@ -1,2 +0,0 @@
-var promptbox = "hey there i'm flappybird<br>";
-document.write(promptbox + promptbox.fontcolor("white"));
