60ff7d29636b19f446847ba46cc543d6bdb0406a
changhan <changhanlee@gmail.com>
2014-06-06 11:40:05 -0400

    shit

--- flappybird.js
+++ flappybird.js
@@ -1,3 +1,9 @@
+/*
+DO NOT USE ANIMATION FOR MOVEMENT
+NEED RECHECKING OF POSITION
+*/
+
+
 stage.x = 1000;
 stage.y = 1000;
 
@@ -15,39 +21,39 @@ intro.addTo(stage).attr({
 var end = new Text('over');
 
 var highpipeypos = 0;
-var highpipe = new Rect (250,highpipeypos,50,400);
+var highpipe = new Rect (250,highpipeypos,20,200);
 highpipe.addTo(stage);
-highpipe.fill('green');
+highpipe.fill('#00FFFF');
 
-var lowpipe = new Rect (250,600,50,400);
+var lowpipe = new Rect (250,300,20,200);
 lowpipe.addTo(stage);
-lowpipe.fill('green');
+lowpipe.fill('#00FFFF');
 
-var bird = new Circle(15,500,15);
+var bird = new Circle(15,200,5);
 bird.addTo(stage);
-bird.fill('white');
+bird.fill('#FFFF00');
 bird.x = 15;
-bird.y = 500;
+bird.y = 200;
 
-
-var gravity = new Animation('1s',{
-	y:1000
+/*
+var gravity = new Animation('6s',{
+	y:400
 });
 gravity.addSubjects([bird]);
 gravity.play();
+*/
 
-
-
+/*
 var jump = new Animation('1s',{
 	y:0
 });
-
+*/
 jump.addSubjects([bird]);
 
 stage.on('keydown', function(e) {
     if(e.keyCode == 65){
     	console.log('hit');
-    	jump.play();
+    	//jump.play();
     	console.log(bird.y);
     }
   });
