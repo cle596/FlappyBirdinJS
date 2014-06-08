8da0593ba0fad10dc7c89313de3f9190a72817ce
changhan <changhanlee@gmail.com>
2014-06-08 10:05:32 -0400

    shit

--- flappybird.js
+++ flappybird.js
@@ -1,10 +1,29 @@
+// 500 x 500
+
+var bg = new Rect (0,0,500,500);
+bg.fill('green');
+bg.addTo(stage);
+
+var title = new Text();
+title.addTo(stage);
+title.attr({
+	text: "fappy brrr press a",
+	textFillColor: 'yellow'
+});
+
 //highpipe
 var highpipe = new Rect (250,0,20,200);
+highpipe.attr({
+	height: Math.floor(Math.random() * 250)
+});
 highpipe.addTo(stage);
 highpipe.fill('#00FFFF');
 
 //lowpipe
-var lowpipe = new Rect (250,300,20,200);
+var lowpipe = new Rect (250,500,20,-200);
+lowpipe.attr({
+	height: - Math.floor(Math.random() * 250)
+});
 lowpipe.addTo(stage);
 lowpipe.fill('#00FFFF');
 
@@ -13,7 +32,21 @@ var bird = new Circle(15,200,5);
 bird.addTo(stage);
 bird.fill('#FFFF00');
 
-var increment = 100;
+//console.log(Object.getOwnPropertyNames(bird.attr).sort());
+
+//var increment = 100;
+
+function rightedge(){
+	if(bird._attributes.x + 5 >= 500 ){
+		//console.log("right edge touched");
+	}
+}
+
+function bottomedge(){
+	if(bird._attributes.y + 5 >= 500 ){
+		//console.log("bottom edge touched");
+	}
+}
 
 function persist(x,y){
 	bird.attr({
@@ -39,6 +72,8 @@ setInterval(function(){this.bird.attr.x = 500;
 
 stage.on('tick', function() {
     persist(bird._attributes.x,bird._attributes.y);
+    rightedge();
+    bottomedge();
 });
 
 
